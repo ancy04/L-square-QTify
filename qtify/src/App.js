f015f831b37504b29e2038b751889998b97bb058
Ancy Sherin A <ancy.a01@ad.infosys.com>
2025-05-02 12:22:20 +0530

    After modification

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { MusicContext } from "./MusicContext";
+
+import { Routes, Route } from "react-router-dom";
+import AlbumDetails from "./components/AlbumDetails/AlbumDetails";
+import Home from "./components/Home/Home";
+
+export const fetchData = async (type) => {
+  try {
+    let res = await axios.get("https://qtify-backend-labs.crio.do/" + type);
+
+    return res.data;
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 function App() {
+  const [newData, setNewData] = useState([]);
+  const [topData, setTopData] = useState([]);
+  const [selectedSong, setSelectedSong] = useState(null);
+
+  useEffect(() => {
+    let arr = [
+      { data: "albums/new", setData: setNewData },
+      { data: "albums/top", setData: setTopData },
+    ];
+    arr.forEach(async (item) => {
+      let data = await fetchData(item.data);
+      await item.setData(data);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MusicContext.Provider
+      value={{ selectedSong, setSelectedSong }}
+    >
+      <div className="App">
+        <Navbar searchData={newData.concat(topData)} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/albumdetails/:id" element={<AlbumDetails />} />
+        </Routes>
+      </div>
+    </MusicContext.Provider>
   );
 }
 
